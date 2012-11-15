ddee217330e0e642a5be32b6109e38ea0d91c937
none <alexander.podkidyshev@gmail.com>
2012-11-15 14:12:52 +0200

    Added function for checking pages in yandex index yap()

--- lib/seo_params/yandex.rb
+++ lib/seo_params/yandex.rb
@@ -16,6 +16,35 @@ module SeoParams
       tic.to_s.to_i
     end
 
+    def yap
+       pages = ask_yandex(@url)
+      (pages.is_a? String) ? (@url = pages; pages = ask_yandex(pages); ) : pages
+      pages
+    end
+
+    private
+      def ask_yandex(url)
+
+        doc = Nokogiri::HTML(open("http://webmaster.yandex.ua/check.xml?hostname=#{url}"))
+
+        if doc.css('div.error-message').length > 0
+          if doc.css('div.error-message').to_s.scan('Сайт является зеркалом')
+            url_regexp = /\?hostname\=([a-zA-Z\.0-9]{1,})/
+            new_url = doc.css('div.error-message').to_s.scan(url_regexp)[0][0]
+            index = 'http://' + new_url
+          end
+
+        else
+          doc.css('div.header div').each do |link|
+              pages = /\d{1,}/
+              @pages_in_index = link.content.scan(pages)
+              index = @pages_in_index[0].to_i
+          end
+        end
+
+        index
+      end
+
   end
 
 end
