d910f32dccdef52d43bbb30b00245ed5cc770075
none <alexander.podkidyshev@gmail.com>
2012-11-15 13:58:37 +0200

    Changed regexp in google_pages()

--- lib/seo_params/google.rb
+++ lib/seo_params/google.rb
@@ -16,7 +16,7 @@ module SeoParams
 
     def google_pages
       doc = Nokogiri::HTML(open("https://www.google.com/search?hl=en&tab=ww&safe=active&tbo=d&sclient=psy-ab&q=site:#{@url}&oq=site:#{@url}"))
-      pages = doc.css('div[@id="resultStats"]').to_s[/\d+/] if doc.css('div[@id="subform_ctrl"]')
+      pages = doc.css('div[@id="resultStats"]').to_s[/[\d,]+/] if doc.css('div[@id="subform_ctrl"]')
       pages ? pages : 0
     end
 
